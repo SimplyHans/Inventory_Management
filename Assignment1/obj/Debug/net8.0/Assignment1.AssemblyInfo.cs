c6aec0ffec4ece90d52c8e7237ddd2b6db499592
SimplyHans <hans.casilao@gmail.com>
2025-04-11 14:47:44 -0400

    final version - local edition

--- Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e08333358c89aac59c107b744e1e427dea82336")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b9639b3c09bb83a3aa1a328d28162380515220a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
