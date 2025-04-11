88c12c458c326356d5f07951819d6cdfdeca7b24
SimplyHans <hans.casilao@gmail.com>
2025-04-11 16:07:30 -0400

    final azure version

---
+++ Assignment1/Migrations/20250411195948_removeIdentitySchema.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assignment1.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeIdentitySchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "UserTokens",
+                schema: "Identity",
+                newName: "UserTokens");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                schema: "Identity",
+                newName: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "UserRoles",
+                schema: "Identity",
+                newName: "UserRoles");
+
+            migrationBuilder.RenameTable(
+                name: "UserLogins",
+                schema: "Identity",
+                newName: "UserLogins");
+
+            migrationBuilder.RenameTable(
+                name: "UserClaims",
+                schema: "Identity",
+                newName: "UserClaims");
+
+            migrationBuilder.RenameTable(
+                name: "Roles",
+                schema: "Identity",
+                newName: "Roles");
+
+            migrationBuilder.RenameTable(
+                name: "RoleClaims",
+                schema: "Identity",
+                newName: "RoleClaims");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                schema: "Identity",
+                newName: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "Orders",
+                schema: "Identity",
+                newName: "Orders");
+
+            migrationBuilder.RenameTable(
+                name: "OrderItems",
+                schema: "Identity",
+                newName: "OrderItems");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                schema: "Identity",
+                newName: "Categories");
+
+            migrationBuilder.RenameTable(
+                name: "CartItems",
+                schema: "Identity",
+                newName: "CartItems");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "UserTokens",
+                newName: "UserTokens",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "Users",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "UserRoles",
+                newName: "UserRoles",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "UserLogins",
+                newName: "UserLogins",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "UserClaims",
+                newName: "UserClaims",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "Roles",
+                newName: "Roles",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "RoleClaims",
+                newName: "RoleClaims",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "Products",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "Orders",
+                newName: "Orders",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "OrderItems",
+                newName: "OrderItems",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "Categories",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "CartItems",
+                newName: "CartItems",
+                newSchema: "Identity");
+        }
+    }
+}
--- Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6aec0ffec4ece90d52c8e7237ddd2b6db499592")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+886fe8acda3ecc3f16ac6b15bd3e5bf8f3b84440")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
