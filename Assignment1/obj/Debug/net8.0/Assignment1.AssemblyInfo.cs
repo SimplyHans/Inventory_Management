7d62dcffe9736dc30d3be3a36fccac6ac7f07a4f
SimplyHans <hans.casilao@gmail.com>
2025-05-09 22:10:55 -0400

    minor UI improvements

--- Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08c32297f088a7615cfe2fc5776f2ae1f13d1889")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9caf69a655affb0ef782892f9e8eae92b85a73f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
